552413bbe3f14a4d47c2a0989a7a1df2d8f48f2a
kkalyankumar9 <kummarikalyan010@gmail.com>
2024-03-19 12:27:09 +0530

    productspage css

--- task2/src/Components/MainOne.jsx
+++ task2/src/Components/MainOne.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, VStack, Button,Input } from "@chakra-ui/react";
+import { Box, VStack, Button, Input } from "@chakra-ui/react";
 import "./MainOne.css";
 import { Search2Icon, SearchIcon } from "@chakra-ui/icons";
 import { IoMdHome } from "react-icons/io";
@@ -10,19 +10,17 @@ import InsideBox from "./InsideBox/InsideBox";
 import { BsStars } from "react-icons/bs";
 import { FiPlusCircle } from "react-icons/fi";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import Products from "./ProductPage.jsx/Products";
 const MainComponent = () => {
-  const [inputValue,setInputValue]=useState("")
+  const [inputValue, setInputValue] = useState("");
   const navigate = useNavigate();
-  
 
   const handleClick = () => {
     // Navigate to a different route programmatically
-    if(!inputValue){
-      alert("Input was Empty!")
+    if (!inputValue) {
+      alert("Input was Empty!");
     }
-   
   };
   return (
     <>
@@ -37,18 +35,26 @@ const MainComponent = () => {
           </VStack>
         </Box>
         <Box className="mainBar">
-          {!inputValue ?<InsideBox/>:<Products/>}
-          <Box  className="searchbar">
-          <FiPlusCircle color="white" className="seico"/>
-            <Input type="text"  placeholder="Ask me anything..." value={inputValue} onChange={(e)=>setInputValue(e.target.value)} color={"white"}/>
-          
-           
-            <Button className="sebut" onClick={handleClick}>  <span><BsStars color="black"/></span>Shop</Button>
-          
+          {!inputValue ? <InsideBox /> : <Products />}
+          <Box className="searchbar">
+            <FiPlusCircle color="white" className="seico" />
+            <Input
+              type="text"
+              placeholder="Ask me anything..."
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              color={"white"}
+            />
 
+            <Button className="sebut" onClick={handleClick}>
+              {" "}
+              <span>
+                <BsStars color="black" />
+              </span>
+              Shop
+            </Button>
+          </Box>
         </Box>
-        </Box>
-      
       </Box>
     </>
   );
